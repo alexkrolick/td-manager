ce41cc92eaf6fd156903d61258d49696aae63f5f
Alex Krolick <amk283@cornell.edu>
2013-03-19 12:47:21 -0400

    backend support for multiple lists

--- todo.py
+++ todo.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-todo_file = 'todo.txt' # filepath
-archive_file = 'archive.txt'
+todo_file = "todo.txt" # filepath
+archive_file = "archive.txt"
 arc = open(archive_file, 'a') # open archive
-t = open(todo_file, 'r') # open active list
 from datetime import datetime
 import readline # for raw_input, allows up-arrowing
 
 class tasklist:
-    def __init__(self,contents=None):
+    def __init__(self,src="todo.txt",contents=None):
         self.contents = contents or []
+        self.src = src
 
     def __str__(self):
         n = 0
@@ -20,74 +20,75 @@ class tasklist:
         return result
 
     def read(self):
+        t = open(self.src, 'r') # open active list
         for line in t:
             self.contents.append(line)
+        t.close()
 
-    def finish(self,i):
+    def finish(self):
+        i = input('num: ')
         timestamp = str(datetime.now())
         arc.write(timestamp+"\t")
         arc.write(self.contents[i]+"\n")
         self.contents.pop(i)
-        tw = open(todo_file, 'w') # use different local variable name
+        tw = open(self.src, 'w') # local name for todo file
         for element in self.contents:
             tw.write(element)
         tw.close()
 
-    def add(self,task_item):
+    def add(self):
+        task_item = raw_input('add: ')
         self.contents.append(task_item)
-        ta = open(todo_file, 'a') # use different local variable name
+        ta = open(self.src, 'a')
         ta.write(task_item+"\n")
         ta.close()
 
-    def edit(self,task_index, task_edit):
+    def edit(self):
+        task_index = input('num: ')
+        task_edit = raw_input('new: ')
         self.contents[task_index] = task_edit + "\n"
-        tw = open(todo_file, 'w') # use different local variable name
+        tw = open(self.src, 'w')
         for element in self.contents:
             tw.write(element)
         tw.close()
 
-    def move(self,i_s,i_e):
+    def move(self):
+        i_s = input('num: ')
+        i_e = input('to: ')
         T = self.contents.pop(i_s)
         self.contents.insert(i_e,T)
-        tw = open(todo_file, 'w') # use different local variable name
+        tw = open(self.src, 'w')
         for element in self.contents:
             tw.write(element)
         tw.close()
 
-tl = tasklist()
-
-tl.read()
-t.close()
+tl = [tasklist("todo.txt")] # default tasklist, src = todo.txt in this directory
+tl[0].read()
 print "To Do:\n"
-print tl
+print tl[0]
 
+# user interactions
 action = raw_input('(f)inished, (a)dd, (e)dit, (m)ove, (h)elp, [enter] to close: ')
 if action == "f":
-    index = input('num: ')
-    tl.finish(index)
+    tl[0].finish()
     print 'removed'
-    print tl
+    print tl[0]
 elif action == "h":
     print "\nTo archive a task:\n Enter 'f' and then the number of the task you want to mark as finished."
     print " The text of the task will be timestamped and copied into the archive file."
     print "\nTo add a task:\n Enter'a' and then the text of the new item."
     print "\nTo edit a task:\n Enter 'e' and then the number of the task you want to change."
 elif action == "a":
-    item = raw_input('add: ')
-    tl.add(item)
+    tl[0].add()
     print 'added'
-    print tl
+    print tl[0]
 elif action == "e":
-    index = input('num: ')
-    edit = raw_input('new: ')
-    tl.edit(index, edit)
+    tl[0].edit()
     print 'changed'
-    print tl
+    print tl[0]
 elif action == "m":
-    index_start = input('num: ')
-    index_end = input('to: ')
-    tl.move(index_start, index_end)
+    tl[0].move()
     print 'moved'
-    print tl
+    print tl[0]
 
 arc.close()
